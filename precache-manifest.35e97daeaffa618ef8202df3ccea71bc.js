701c57cff3c94b63edeafab077741ecb3f957b1c
shamsKhokhar56 <shamsudinkhokhar56@gmail.com>
2020-08-28 00:02:17 +0500

    Updates

---
+++ precache-manifest.35e97daeaffa618ef8202df3ccea71bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24feaa66d3f13b8ab2feaec45ef714b7",
+    "url": "/monsRolo/index.html"
+  },
+  {
+    "revision": "1bba8107c4ef5bb1ccc0",
+    "url": "/monsRolo/static/css/main.c9fd418b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsRolo/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsRolo/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bba8107c4ef5bb1ccc0",
+    "url": "/monsRolo/static/js/main.56c9381f.chunk.js"
+  },
+  {
+    "revision": "b88440fc7319821d9a02",
+    "url": "/monsRolo/static/js/runtime-main.23a41346.js"
+  }
+]);
